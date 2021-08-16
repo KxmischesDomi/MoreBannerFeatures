2d1e18d675f19369b9e9822cc3bad827a229d359
KxmischesDomi <kxmischesdomi@gmail.com>
2021-08-16 18:44:33 +0200

    Add Trader LLama Offsets

--- src/main/java/de/kxmischesdomi/morebannerfeatures/mixin/trader/TraderLLamaEntityMixin.java
+++ src/main/java/de/kxmischesdomi/morebannerfeatures/mixin/trader/TraderLLamaEntityMixin.java
@@ -1,24 +1,14 @@
 package de.kxmischesdomi.morebannerfeatures.mixin.trader;
 
-import de.kxmischesdomi.morebannerfeatures.common.morebannerfeatures.SideBannerable;
 import de.kxmischesdomi.morebannerfeatures.common.morebannerfeatures.InventoryBannerable;
-import de.kxmischesdomi.morebannerfeatures.utils.BannerUtils;
-import net.minecraft.entity.EntityData;
+import de.kxmischesdomi.morebannerfeatures.common.morebannerfeatures.SideBannerable;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
 import net.minecraft.entity.passive.TraderLlamaEntity;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.world.LocalDifficulty;
-import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * NOT IMPLEMENTED!!!
  *
  * @author KxmischesDomi | https://github.com/kxmischesdomi
  * @since 1.0
@@ -28,19 +18,6 @@ public abstract class TraderLLamaEntityMixin extends AbstractDonkeyEntity implem
 
 	public TraderLLamaEntityMixin(EntityType<? extends AbstractDonkeyEntity> entityType, World world) {
 		super(entityType, world);
-	}
-
-	@Inject(method = "initialize", at = @At("HEAD"))
-	private void initEquipment(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData, NbtCompound entityNbt, CallbackInfoReturnable<EntityData> cir) {
-//		if (spawnReason == SpawnReason.EVENT) {
-			this.initEquipment(difficulty);
-//		}
-	}
-
-	@Override
-	protected void initEquipment(LocalDifficulty difficulty) {
-		items.setStack(getSlot(), BannerUtils.getRandomBannerItemStack(random));
-		super.initEquipment(difficulty);
 	}
 
 	@Override
