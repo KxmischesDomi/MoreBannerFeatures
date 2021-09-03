864848355eea4e669395d1915f253db02a0c2e8a
KxmischesDomi <kxmischesdomi@gmail.com>
2021-09-03 14:48:42 +0200

    Replace Redirect with Inject

--- src/main/java/de/kxmischesdomi/morebannerfeatures/mixin/player/CapeFeatureRendererMixin.java
+++ src/main/java/de/kxmischesdomi/morebannerfeatures/mixin/player/CapeFeatureRendererMixin.java
@@ -2,13 +2,16 @@ package de.kxmischesdomi.morebannerfeatures.mixin.player;
 
 import de.kxmischesdomi.morebannerfeatures.common.morebannerfeatures.Bannerable;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.CapeFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * @author KxmischesDomi | https://github.com/kxmischesdomi
@@ -21,9 +24,11 @@ public abstract class CapeFeatureRendererMixin extends FeatureRenderer<AbstractC
 		super(context);
 	}
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;canRenderCapeTexture()Z"))
-	public boolean canRenderCapeTexture(AbstractClientPlayerEntity abstractClientPlayerEntity) {
-		return abstractClientPlayerEntity.canRenderCapeTexture() && !hasCustomBanner(abstractClientPlayerEntity);
+	@Inject(method = "render", at = @At(value = "HEAD"), cancellable = true)
+	public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, AbstractClientPlayerEntity abstractClientPlayerEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
+		if (hasCustomBanner(abstractClientPlayerEntity)) {
+			ci.cancel();
+		}
 	}
 
 	public boolean hasCustomBanner(AbstractClientPlayerEntity playerEntity) {
