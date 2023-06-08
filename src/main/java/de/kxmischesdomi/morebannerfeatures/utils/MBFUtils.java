8e85bd002396c19bba69f92f643ab03e59c409a7
KxmischesDomi <kxmischesdomi@gmail.com>
2023-06-08 21:25:49 +0200

    fix trinkets

--- src/main/java/de/kxmischesdomi/morebannerfeatures/utils/MBFUtils.java
+++ src/main/java/de/kxmischesdomi/morebannerfeatures/utils/MBFUtils.java
@@ -13,18 +13,18 @@ import java.util.Optional;
 public class MBFUtils {
 
 	public static ItemStack getCloakItem(Player player) {
-//		if (MoreBannerFeatures.isTrinketsInstalled()) {
-//			try {
-//				Optional<dev.emi.trinkets.api.TrinketComponent> component = dev.emi.trinkets.api.TrinketsApi.getTrinketComponent(player);
-//
-//				if (component.isPresent()) {
-//					return component.get().getInventory().get("chest").get("cape").getItem(0);
-//				}
-//			} catch (Exception exception) {
-//				// TRINKETS IS PROBABLY NOT INSTALLED ON SERVER
-//			}
-//
-//		}
+		if (MoreBannerFeatures.isTrinketsInstalled()) {
+			try {
+				Optional<dev.emi.trinkets.api.TrinketComponent> component = dev.emi.trinkets.api.TrinketsApi.getTrinketComponent(player);
+
+				if (component.isPresent()) {
+					return component.get().getInventory().get("chest").get("cape").getItem(0);
+				}
+			} catch (Exception exception) {
+				// TRINKETS IS PROBABLY NOT INSTALLED ON SERVER
+			}
+
+		}
 		return player.getInventory().getArmor(2);
 	}
 
